d8dc1bf38cd5b49f678ec7b11fa0dd1d209beaa7
Tlapr231 <tlapr093@uottawa.ca>
2020-03-06 10:49:42 -0500

    User-Detail component integration. Final tut3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UsersComponent ],
+  declarations: [ AppComponent, HelloComponent, UsersComponent, UserDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
