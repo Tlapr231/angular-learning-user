65eb2dae21491abf89c381bd75fc392240adecaa
Tlapr231 <tlapr093@uottawa.ca>
2020-03-11 11:53:12 -0400

    CSS Update

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>Welcome to the {{name}}</h1>`,
+  styles: [`h1 { font-family: Lato; text-align: center}`]
 })
 export class HelloComponent  {
   @Input() name: string;
