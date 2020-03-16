bee802da93c587f267c7135339f3c8b4a89181de
Tlapr231 <tlapr093@uottawa.ca>
2020-03-16 09:04:28 -0400

    keypress event handler added to detail form

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import { Subscription } from 'rxjs/Subscription';
 
 import { User } from "../user";
 import { UserService } from "../user.service"; 
+
+const ENTER = 13;
 
 @Component({
   selector: 'app-user-detail',
@@ -13,6 +18,7 @@ import { UserService } from "../user.service";
 export class UserDetailComponent implements OnInit {
 
   @Input() user: User;
+  subsciption: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +28,17 @@ export class UserDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUser();
+    this.subsciption = Observable.fromEvent(document, 'keypress').subscribe(e => {
+      if (e.keyCode === ENTER){
+        //if enter is pressed.
+        console.log("Enter Key Pressed");
+        this.save();
+      }
+    })
+  }
+
+  ngOnDestroy(){
+    this.subsciption.unsubscribe();
   }
 
   getUser(): void {
