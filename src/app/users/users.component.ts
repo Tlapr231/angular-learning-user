6029645f5dfe7b1cca94ca369ba6924669639b5d
Tlapr231 <tlapr093@uottawa.ca>
2020-03-17 13:55:14 -0400

    the accounts are being pulled but not instantly which makes my code fail

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,7 +8,7 @@ import { UserService } from "../user.service";
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
-})
+}) 
 
 export class UsersComponent implements OnInit {
 
